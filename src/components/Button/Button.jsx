6f4ced77f00658dfbd0441d51fd0f50baa714ad1
Marcello <marcello_aragoni@yahoo.com.br>
2022-04-18 12:25:09 -0300

    Resolved discussion

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,13 +2,13 @@ import cn from "classnames";
 
 import "./Button.scss";
 
-export default function Button({text, icon, color}) {
+export default function Button({ text, icon, color }) {
   const buttonStyle = cn(
     "button",
-    {"button__icon--plus": icon === "plus"},
-    {"button__icon--minus": icon === "minus"},
-    {"button__icon--add": icon === "add"},
-    {"button--orange": color === "orange"}
+    { "button__icon--plus": icon === "plus" },
+    { "button__icon--minus": icon === "minus" },
+    { "button__icon--add": icon === "add" },
+    { "button--orange": color === "orange" }
   );
 
   return (
