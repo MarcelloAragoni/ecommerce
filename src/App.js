fa5665c8836ce9d3b9c2af233c05756bcf9387f9
Marcello <marcello_aragoni@yahoo.com.br>
2022-04-18 17:55:06 -0300

    final adjustments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <>
+      <ProductPage />
       <Button icon="plus" />
       <Button icon="minus" />
       <Button icon="add" color="orange" text="Add to cart" />
