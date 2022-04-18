4c338cd3c0b48a8a0bcc703b6660a846de15bf97
Marcello <marcello_aragoni@yahoo.com.br>
2022-04-18 10:44:30 -0300

    class rename

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,9 +5,9 @@ import "./Button.scss";
 export default function Button({text, icon, color}) {
   const buttonStyle = cn(
     "button",
-    {"button--plus": icon === "plus"},
-    {"button--minus": icon === "minus"},
-    {"button--add": icon === "add"},
+    {"button__icon--plus": icon === "plus"},
+    {"button__icon--minus": icon === "minus"},
+    {"button__icon--add": icon === "add"},
     {"button--orange": color === "orange"}
   );
 
