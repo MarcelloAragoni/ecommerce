428f8f188c0d12471db013da13a95e51f87c44b5
Marcello <marcello_aragoni@yahoo.com.br>
2022-04-25 09:43:59 -0300

    created integration with data.json and index style.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import "./App.css";
-import ProdDetail from "./pages/ProductPage/ProdDetail/ProdDetail";
+import ProductPage from "./pages/ProductPage";
+import "./reset.css";
 
 function App() {
-  return (
-    <>
-      <ProdDetail
-        company="SNEAKER COMPANY"
-        model="Fall Limited Edition Sneakers"
-        description="These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer."
-        price="$125.00"
-        discount="50%"
-        originalPrice="$250.00"
-      />
-    </>
-  );
+  return <ProductPage />;
 }
 
 export default App;
--- src/pages/ProductPage/index.jsx
+++ src/pages/ProductPage/index.jsx
@@ -1,3 +1,39 @@
+import { useEffect, useState } from "react";
+import ProdDetail from "./ProdDetail/ProdDetail";
+import ProdSelector from "./ProdSelector/ProdSelector";
+
+import "./index.scss";
+
 export default function ProductPage() {
-  return <h1>product ProductPage</h1>;
+  const [products, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setProduct(data.products);
+      });
+  }, []);
+
+  return (
+    <div className="product-page">
+      <div className="product-galerry">
+        <p>gallery here</p>
+      </div>
+      <div className="product-info">
+        {products.map((product) => (
+          <ProdDetail
+            key={product.id}
+            company={product.company}
+            model={product.model}
+            description={product.description}
+            price={product.price}
+            originalPrice={product.originalprice}
+            discount={product.discount}
+          />
+        ))}
+        <ProdSelector />
+      </div>
+    </div>
+  );
 }
