f34a448e593149b6065db4c8e63fb337f479fe2c
MarcelloAragoni <32502447+MarcelloAragoni@users.noreply.github.com>
2022-05-09 19:13:26 -0300

    Logo/image component (#8)
    **Trello:** https://trello.com/c/pFaFRHnB/5-product-view-page
    
    ## Description
    
    created logo image component
    
    ## Screenshot
    ![image](https://user-images.githubusercontent.com/32502447/167468181-7b36c09c-3798-4266-bb49-fb23df519a9f.png)

---
+++ src/components/Images/Images.jsx
@@ -0,0 +1,20 @@
+import cn from "classnames";
+
+export default function Images({ source, onClick, imagetype }) {
+  const IMAGE_PATH = "/images/";
+  const imgtype = cn(
+    "image",
+    { "user-image": imagetype === "user" },
+    { "gallery-image": imagetype === "gallery" },
+    { "logo-image": imagetype === "logo" }
+  );
+
+  return (
+    <img
+      className={imgtype}
+      onClick={onClick}
+      src={IMAGE_PATH + source}
+      alt=""
+    />
+  );
+}
