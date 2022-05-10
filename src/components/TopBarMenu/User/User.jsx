7d387a4049dc04392503dd996207a22f858bdef0
Marcello <marcello_aragoni@yahoo.com.br>
2022-05-10 08:48:07 -0300

    add alt property to images component.

--- src/components/TopBarMenu/User/User.jsx
+++ src/components/TopBarMenu/User/User.jsx
@@ -3,5 +3,7 @@ import Images from "../../Images/Images";
 import "./User.scss";
 
 export default function User() {
-  return <Images imagetype="user" source="image-avatar.png" />;
+  return (
+    <Images alt="User Avatar" imagetype="user" source="image-avatar.png" />
+  );
 }
